6321594cd359198ddcf979fa51cd6f6eb1190371
Thassia <thassiadev@gmail.com>
2023-02-25 23:38:40 +0100

    initial commit - project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='Pomodoro Bot',
+      version='0.1.0',
+      description='a pomodoro counter discord BOT',
+      author='Thassia Devilart',
+      author_email='thassiadev@gmail.com',
+      url='https://github.com/ThassiaDevilart/Pomodoro-BOT.git',
+      packages=[],
+     )
---
+++ source/main.py
@@ -0,0 +1,8 @@
+"""Main Pomodoro Program"""
+import sys
+def main():
+    """Main entry point for the script."""
+    print("hello world")
+
+if __name__ == '__main__':
+    sys.exit(main())
