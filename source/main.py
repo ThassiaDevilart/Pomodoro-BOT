3cd4dc8339312e6afe18c5ec5f745f56a9e62ce9
Thassia <thassiadev@gmail.com>
2023-03-12 02:00:49 +0100

    class structure

--- source/main.py
+++ source/main.py
@@ -1,5 +1,42 @@
 """Main Pomodoro Program"""
 import sys
+
+class Timer:
+    def __init__(self):
+        # todo
+        pass
+
+
+class Bot:
+    def __init__(self):
+        #todo
+        pass
+
+
+class PomodoroState:
+    def __init__(self):
+         #todo
+         pass
+
+
+class PmodoroSetting:
+    def __init__(self):
+        #todo
+        pass
+
+
+class CLI:
+    def __init__(self):
+        #todo
+        pass
+
+
+class PomodoroClient:
+    def __init__(self):
+        #todo
+        pass
+
+
 def main():
     """Main entry point for the script."""
     print("hello world")
